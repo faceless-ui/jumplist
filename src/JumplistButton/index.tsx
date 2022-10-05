93de848acccd7baf3dd9d2324704d3e66e68fcd8
Jacob Fletcher <jacobsfletch@gmail.com>
2022-10-04 23:31:46 -0400

    chore: requires nodeID in JumplistButton

--- src/JumplistButton/index.tsx
+++ src/JumplistButton/index.tsx
@@ -1,72 +1,62 @@
-import React, { HTMLProps, MouseEvent, useCallback } from 'react';
-import { useJumplist } from '../JumplistProvider/context';
+import React, { HTMLProps, MouseEvent, useCallback, useEffect } from 'react';
+import { JumplistNode, useJumplist } from '../JumplistProvider/context';
 
 export interface JumplistButtonProps extends HTMLProps<HTMLElement> {
   htmlElement?: React.ElementType
   children?: React.ReactNode
   direction?: 'prev' | 'next'
-  nodeID?: string
+  nodeID: string
+  activeClassName?: string
 }
 
 export const JumplistButton: React.FC<JumplistButtonProps> = (props) => {
   const {
     htmlElement: Tag = 'button',
     children,
-    direction,
     nodeID,
     onClick,
+    activeClassName,
+    className,
     ...rest
   } = props;
 
   const {
     scrollToID,
     jumplist,
-    activeJumplistIndex,
   } = useJumplist();
 
+  const [nodeState, setNodeState] = React.useState<JumplistNode | undefined>(() => jumplist?.find((item) => item.nodeID === nodeID));
+
+  useEffect(() => {
+    setNodeState(jumplist?.find((item) => item.nodeID === nodeID));
+  }, [
+    jumplist,
+    nodeID
+  ]);
+
   const handleClick = useCallback((e: MouseEvent<HTMLElement>) => {
-    if (activeJumplistIndex !== undefined) {
-      if (direction === 'prev') {
-        const prevItem = jumplist[activeJumplistIndex - 1];
-        if (prevItem) {
-          scrollToID(prevItem.nodeID);
-        }
-      }
-
-      if (direction === 'next') {
-        const nextItem = jumplist[activeJumplistIndex + 1];
-        if (nextItem) {
-          scrollToID(nextItem.nodeID);
-        }
-      }
-
-      if (!direction && nodeID !== undefined) {
-        scrollToID(nodeID);
-      }
+    if (nodeID) {
+      scrollToID(nodeID);
     }
 
     if (typeof onClick === 'function') {
       onClick(e);
     }
   }, [
-    direction,
     scrollToID,
     onClick,
-    jumplist,
-    activeJumplistIndex,
     nodeID
   ]);
-
-  let ariaLabel = direction === 'prev' ? 'Scroll to previous item' : 'Scroll to next item';
-  if (!direction && nodeID !== undefined) {
-    ariaLabel = `Scroll to ${nodeID}`;
-  }
 
   return (
     <Tag
       id={nodeID ? `jumplist-button_${nodeID}` : undefined}
       type="button"
-      aria-label={ariaLabel}
+      aria-label={`Scroll to ${nodeID}`}
+      className={[
+        className,
+        nodeState?.isIntersecting && activeClassName
+      ].filter(Boolean).join(' ')}
       {...rest}
       onClick={handleClick}
     >
